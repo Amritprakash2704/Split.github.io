35e96e152afca5de48ad7a5c12afb152bc13694c
Amrit Prakash <amritdevil1st@gmail.com>
2022-03-14 18:14:14 +0530

    Completed Dashboard Front-end

---
+++ Website/Javascript/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCyTYhrssyWhM1P6db_ZS_UYXHzCEupX8g",
+  authDomain: "split-662dd.firebaseapp.com",
+  projectId: "split-662dd",
+  storageBucket: "split-662dd.appspot.com",
+  messagingSenderId: "418787962459",
+  appId: "1:418787962459:web:ef0be705d33a524630997e",
+  measurementId: "G-9ERQKCWY34"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
